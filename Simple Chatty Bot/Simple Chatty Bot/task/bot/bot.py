f09d09cb61bddd22fe5dc623f0e159f74b2c85ad
nazrinsh <93432076+nazrinsh@users.noreply.github.com>
2022-01-05 14:38:09 +0400

    Update bot.py

--- Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
@@ -2,36 +2,36 @@ from smtpd import program
 
 
 def greet(bot_name, birth_year):
-    print('Hello! My name is ' + bot_name + '.')
-    print('I was created in ' + birth_year + '.')
+    print(f"Hello! My name is {bot_name}.")
+    print(f"I was created in {birth_year}.")
 
 
 def remind_name():
-    print('Please, remind me your name.')
+    print("Please, remind me your name.")
     name = input()
-    print('What a great name you have, ' + name + '!')
+    print(f"What a great name you have {name}.")
 
 
 def guess_age():
-    print('Let me guess your age.')
-    print('Enter remainders of dividing your age by 3, 5 and 7.')
+    print("Let me guess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
 
-    rem3 = int(input())
-    rem5 = int(input())
-    rem7 = int(input())
-    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+    remainder3 = int(input())
+    remainder5 = int(input())
+    remainder7 = int(input())
+    your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
+    print(f"Your age is {your_age}; that's a good time to start programming!")
 
 
 def count():
-    print('Now I will prove to you that I can count to any number you want.')
-
-    num = int(input())
-    curr = 0
-    while curr <= num:
-        print(curr, '!')
-        curr = curr + 1
+    print("Now I will prove to you that I can count to any number you want.")
+    number = int(input())
+    b = 0
+    while b <= number:
+        print(b, "!")
+        b = b + 1
+    print("Completed have a nice day!")
 
 
 def test():
@@ -40,11 +40,10 @@ def test():
     print("Why do we use methods?\n1. To repeat a statement multiple times.\n2. To decompose a program into several small subroutines.\n3. To determine the execution time of a program.\n4. To interrupt the execution of a program.")
     while True:
         choose = int(input())
-        if choose != 4:
+        if choose != 2:
             print("Please, try again.")
-
         else:
-            print('Completed, have a nice day!')
+            print("Completed, have a nice day!")
             break
 
 
@@ -52,7 +51,7 @@ def end():
     print('Congratulations, have a nice day!')
 
 
-greet('Aid', '2020')  # change it as you need
+greet('A', '2022')  
 remind_name()
 guess_age()
 count()
